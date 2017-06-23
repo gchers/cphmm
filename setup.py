b2bd8176e428ae21c12a59c8dfb1ef23ca910fb0
Giovanni Cherubin <giovanni.cherubin@gmail.com>
2017-06-23 17:49:06 +0100

    Make cphmm package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='cphmm',
+      version='1.0',
+      description='Confidence Prediction for HMM',
+      author='Giovanni Cherubin',
+      author_email='g.chers@gmail.com',
+      packages=['cphmm'],
+     )
